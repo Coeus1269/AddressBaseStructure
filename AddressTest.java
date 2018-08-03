9b4470f6327313ae02df439e09044bf22cd2f267
Will <38478803+Coeus1269@users.noreply.github.com>
2018-08-03 10:16:39 -0500

    Add files via upload
    The class for testing the address structure

---
+++ AddressTest.java
@@ -0,0 +1,14 @@
+package net.wg.address;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddressTest {
+
+	@Test
+	public void testIsAddressValid() {
+		fail("Not yet implemented");
+	}
+
+}
