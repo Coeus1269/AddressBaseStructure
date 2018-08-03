3f543ce5a7696924107d3be7c5288a83ecec3aeb
Will <38478803+Coeus1269@users.noreply.github.com>
2018-08-03 10:09:15 -0500

    Add files via upload

---
+++ address/US_City.java
@@ -0,0 +1,156 @@
+package net.wg.address;
+
+public class US_City 
+{
+	private int CityID;
+	private String cityName;
+
+	private US_State state;
+
+	private US_ZipCode zipCode;
+	
+	private String county;			// TODO need county class?
+	
+	// TODO: Ideally, on city validate this class will pull from a local file or DB for validation of 
+	//  name, spelling, State, zipcode and county
+	// TODO: Method for name misspelling correction based on local file or DB
+	// TODO ability to find city based on zip code
+	// TODO compare city text with misspelled list
+
+	public static void main( String...args )
+		{
+		TestData();
+		}
+	
+	public US_City()
+		{
+		state = new US_State();
+		zipCode = new US_ZipCode();
+		}
+	
+	public US_City(String CityName, String StateName, String ZipCode)
+		{
+			if(CityName != null && CityName.trim().length() > 0)
+				cityName = CityName.trim();
+		
+			if(StateName != null && StateName.trim().length() > 0)
+				state = new US_State(StateName.trim());
+			
+			if(ZipCode != null && ZipCode.trim().length() > 0)
+				zipCode = new US_ZipCode(ZipCode.trim());
+		}
+	
+	public boolean isCityValid()
+		{
+			if(!isCityNameValid(cityName))
+					return false;
+			
+			if (!zipCode.isZip5Valid())
+				return false;
+			
+			if(!state.isStateEmpty())
+				return false;
+			
+			return true;
+		}
+	
+	public boolean isCityNameValid(String CityName)
+		{
+		CityName = CityName.trim();
+			return CityName.matches("(?ix)^[A-Z\\.-]+(?:\\s+[A-Z\\.-]+)*$");
+		}
+
+	
+	public String toString()
+		{
+			return getCityName() + ", " + state.toString() + " " + zipCode.toString();
+		}
+	
+	public static void TestData()
+		{
+			US_City City = new US_City("Oklahoma City", "Oklahoma", "73112");
+			System.out.println(City.toString());
+			
+			City = new US_City("Oklahoma City", "Oklahoma", "73112-5103");
+			System.out.println(City.toString());
+			
+			City = new US_City("Oklahoma City", "OK", "73112-5103");
+			System.out.println(City.toString());
+		}
+	
+	/* ------------------------------ Getters & Setters  ------------------------------ */
+
+	public String getCityName() 
+		{
+			return cityName;
+		}
+
+	public void setCityName(String cityName) 
+		{
+			this.cityName = cityName;
+		}
+
+	public US_State getState() 
+		{
+			return state;
+		}
+	
+	public String getStateName() 
+		{
+			return state.getStateName();
+		}
+	
+	public String  getStateAbbrev() 
+		{
+			return state.getStateAbbreviation();
+		}
+
+	public void setState(US_State state) 
+		{
+			this.state = state;
+		}
+
+	public US_ZipCode getZipCode() 
+		{
+			return zipCode;
+		}
+	
+	public String getZip5() 
+		{
+			return zipCode.getZip5();
+		}
+	
+	public String getZipPlus4() 
+		{
+			return zipCode.getZipPlus4();
+		}
+
+	public void setZipCode(US_ZipCode zipCode) 
+		{
+			this.zipCode = zipCode;
+		}
+
+	public String getCounty() 
+		{
+			return county;
+		}
+
+	public void setCounty(String county) 
+		{
+			this.county = county;
+		}
+
+	public int getCityID() 
+		{
+			return CityID;
+		}
+
+	public void setCityID(int cityID) 
+		{
+			CityID = cityID;
+		}
+	
+	/* ------------------------------ End Getters & Setters  ------------------------------ */
+
+	
+}
---
+++ address/US_State.java
@@ -0,0 +1,254 @@
+package net.wg.address;
+
+import net.wg.utils.FileUtils;
+// import net.wg.utils.StringUtils;
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class US_State 
+{
+	private String StateID;
+	private int stateIndex = 0;
+	private String StatesDataFile = "Data/US_States.txt";
+	private static String[][] AmericanStates = null; // format = Abbreviation, State name
+	// the StatesDataFile file has to keep the same order as the AmericanStates variable
+	// the first element is empty string so if no match is made empty string can be displayed
+	
+	// optional
+	private String stateMotto;		// State Motto String
+	private String stateNickname;
+	private String stateCapitol;
+	private String stateSong;		
+	private String stateSongURl;	
+	private String stateBirdName;
+	private String stateBirdURL;
+	private String stateFlagURL;
+
+	// TODO state spelling errors check	
+	// TODO state data file DB
+	// TODO ability to find state based on zip code
+	// TODO compare state text with misspelled list
+	
+	public static void main( String...args )
+		{
+		TestData();
+		}
+	
+	public US_State()
+		{
+		if(AmericanStates == null || AmericanStates.length < 1)
+			AmericanStates = FileUtils.Load2xStringArrayFromFile(StatesDataFile);
+		}
+	
+	public US_State(String StateString)
+		{
+		if(AmericanStates == null || AmericanStates.length < 1)
+			AmericanStates = FileUtils.Load2xStringArrayFromFile(StatesDataFile);
+		
+		if(StateString != null && StateString.trim().length() > 0)
+			stateIndex = getAmericanStatesIndex(StateString);	
+		}
+	
+		
+	public boolean isStateEmpty()
+		{		
+			//return StringUtils.isEmptyString( stateName );
+			if (stateIndex > 0)
+				return false;
+			else
+				return true;
+		}
+
+	public String toString()
+		{
+			return getStateAbbreviation();
+		}
+	
+	private static int getAmericanStatesIndex(String StateString)
+		{
+		StateString = StateString.trim();
+		if(StateString.length() == 2)
+			return getAmericanStateAbbrevIndex(StateString);
+		else
+			return getAmericanStateNameIndex(StateString);
+
+		}
+	
+	private static int getAmericanStateNameIndex(String StateName)
+		{
+			for (int Index_int=0; Index_int < AmericanStates.length;  Index_int++)
+		        {
+					if(	StateName.equals(AmericanStates[Index_int][1]))
+							{
+							return Index_int;
+							}
+		        }
+			return 0;
+		}
+	
+	private static int getAmericanStateAbbrevIndex(String StateAbbrev)
+	{
+		for (int Index_int=0; Index_int < AmericanStates.length;  Index_int++)
+	        {
+				if(	StateAbbrev.equals(AmericanStates[Index_int][0]))
+						{
+						return Index_int;
+						}
+	        }
+		return 0;
+	}
+	
+	public static void TestData()
+		{
+		
+		US_State State = new US_State();
+		
+		State.setStateIndex(3);
+		System.out.println(State.getStateAbbreviation() + ", " + State.getStateName());
+		
+		State = new US_State("Oklahoma");
+		System.out.println(State.getStateAbbreviation() + ", " + State.getStateName());
+		
+		State = new US_State("TN");
+		System.out.println(State.getStateAbbreviation() + ", " + State.getStateName());
+		
+		State.setStateIndex(0); // confirm that the first element is empty string
+		System.out.println(State.getStateAbbreviation() + ", " + State.getStateName());
+		
+		// list the states and abbreviations		
+//		System.out.println("\nAll states and abbreviations");
+//		for (int Index_int=0; Index_int < AmericanStates.length;  Index_int++)
+//	        {
+//			State.setStateIndex(Index_int);
+//			System.out.println(State.getStateName() + ", " + State.getStateAbbreviation());
+//	        }
+			
+		}
+	
+	/* ------------------------------ Getters & Setters  ------------------------------ */
+
+	public String getStateID() 
+		{
+			return StateID;
+		}
+
+	public void setStateID(String stateID) 
+		{
+			StateID = stateID;
+		}
+
+	public int getStateIndex() 
+		{
+			return stateIndex;
+		}
+
+	public void setStateIndex(int stateIndex) 
+		{
+			this.stateIndex = stateIndex;
+		}
+
+	public String getStateName() 
+		{
+			return AmericanStates[stateIndex][1];
+		}
+	
+	public void setStateName(String StateName) 
+		{
+			//this.stateName = stateName;
+			stateIndex = getAmericanStateNameIndex(StateName);
+		}
+	
+	public String getStateAbbreviation() 
+		{
+		return AmericanStates[stateIndex][0];
+			//return AmericanStates[stateIndex];
+		}
+	
+	public void setStateAbbreviation(String StateAbbreviation) 
+		{
+			//this.stateAbbreviation = stateAbbreviation;
+			stateIndex = getAmericanStatesIndex(StateAbbreviation);
+		}
+	
+	public String getStateMotto() 
+		{
+			return stateMotto;
+		}
+	
+	public void setStateMotto(String stateMotto) 
+		{
+			this.stateMotto = stateMotto;
+		}
+	
+	public String getStateSong() 
+		{
+			return stateSong;
+		}
+	
+	public void setStateSong(String stateSong) 
+		{
+			this.stateSong = stateSong;
+		}
+	
+	public String getStateFlagURL() 
+		{
+			return stateFlagURL;
+		}
+	
+	public void setStateFlagURL(String stateFlagURL) 
+		{
+			this.stateFlagURL = stateFlagURL;
+		}
+
+	public String getStatesDataFile() {
+		return StatesDataFile;
+	}
+
+	public void setStatesDataFile(String statesDataFile) {
+		StatesDataFile = statesDataFile;
+	}
+
+	public String getStateNickname() {
+		return stateNickname;
+	}
+
+	public void setStateNickname(String stateNickname) {
+		this.stateNickname = stateNickname;
+	}
+
+	public String getStateCapitol() {
+		return stateCapitol;
+	}
+
+	public void setStateCapitol(String stateCapitol) {
+		this.stateCapitol = stateCapitol;
+	}
+
+	public String getStateSongURl() {
+		return stateSongURl;
+	}
+
+	public void setStateSongURl(String stateSongURl) {
+		this.stateSongURl = stateSongURl;
+	}
+
+	public String getStateBirdName() {
+		return stateBirdName;
+	}
+
+	public void setStateBirdName(String stateBirdName) {
+		this.stateBirdName = stateBirdName;
+	}
+
+	public String getStateBirdURL() {
+		return stateBirdURL;
+	}
+
+	public void setStateBirdURL(String stateBirdURL) {
+		this.stateBirdURL = stateBirdURL;
+	}
+	
+	/* ------------------------------ End Getters & Setters  ------------------------------ */
+
+	
+}
